c328e5648c31fae4731a10411e61a96c0c771526
3a532008 <isme1788@gmail.com>
2018-10-29 19:30:53 +0800

    Route to HomeController@index

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return redirect('welcome');
-});
+Route::get('/', ['as'=>'home.index', 'uses'=>'HomeController@index']);
+
 
 Route::get('welcome', function () {
     return view('welcome');
