85fc1dc026264eef9715299d91bb3a02f1bce866
3a532008 <isme1788@gmail.com>
2018-10-29 19:33:39 +0800

    設定Route前置 更正

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,15 @@ Route::get('say/{name?}', ['as'=>'hello.index', function ($name='Everybody') {
 
     return 'Hello, '.$name;
 }]);
+Route::get('dashboard', function () {
+    return 'dashboard';
+});
+
 
 Route::group(['prefix'=>'admin'], function () {
     Route::get('dashboard', function () {
-        return 'dashboard';
+        return 'admin dashboard';
+
     });
 
 });
